cb0dad865658da8373f831a0f00c254ca8ef920f
bjxieb <xiebiao@jd.com>
2016-06-01 11:16:20 +0800

    ll Exchanger

---
+++ code/src/main/java/_1/chapter5_18/BigIntegerCompute.java
@@ -0,0 +1,14 @@
+package _1.chapter5_18;
+
+import java.math.BigInteger;
+
+/**
+ * @author bjxieb
+ * @date 6/1/16
+ */
+public class BigIntegerCompute implements Computable<String, BigInteger> {
+  @Override
+  public BigInteger compute(String s) {
+    return null;
+  }
+}
---
+++ code/src/main/java/_1/chapter5_18/Computable.java
@@ -0,0 +1,10 @@
+package _1.chapter5_18;
+
+/**
+ * @author bjxieb
+ * @date 6/1/16
+ */
+public interface Computable<A, V> {
+  V compute(final  A a);
+}
+
