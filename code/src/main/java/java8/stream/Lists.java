ff6968f4881daa5fe40c69e699022e6cc341dfa0
xiebiao <joyrap@gmail.com>
2016-10-16 22:45:50 +0800

    ll

---
+++ code/src/main/java/java8/stream/Lists.java
@@ -0,0 +1,17 @@
+package java8.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author xiebiao
+ * @date 10/16/16
+ */
+public class Lists {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("hello");
+
+    }
+}
---
+++ code/src/main/java/java8/stream/package-info.java
@@ -0,0 +1,4 @@
+package java8.stream;
+/**
+ * http://www.ibm.com/developerworks/cn/java/j-java-streams-1-brian-goetz/index.html
+ */
